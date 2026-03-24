f8d4c7ca0cbedec9b8764361b8be2895b70fb85f
raulmaciel <raulmacielxd@gmail.com>
2026-03-24 15:59:22 -0300

    docs(task): add Swagger documentation for requests DTOs

--- src/main/java/com/totvs/taskmanager/controller/dto/request/CreateTaskRequest.java
+++ src/main/java/com/totvs/taskmanager/controller/dto/request/CreateTaskRequest.java
@@ -1,16 +1,21 @@
 package com.totvs.taskmanager.controller.dto.request;
 
 import com.totvs.taskmanager.entity.TaskEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
+@Schema(description = "Request to create a new task")
 public record CreateTaskRequest(
+        @Schema(description = "Task title", example = "Set up project")
         @NotBlank String title,
 
+        @Schema(description = "Task description", example = "Configure project java")
         String description,
 
+        @Schema(description = "User identifier that owns the task", example = "550e8400-e29b-41d4-a716-446655440000")
         @NotNull(message = "The user identifier is required")
         UUID userId
 ) {
--- src/main/java/com/totvs/taskmanager/controller/dto/request/UpdateTaskStatusRequest.java
+++ src/main/java/com/totvs/taskmanager/controller/dto/request/UpdateTaskStatusRequest.java
@@ -1,9 +1,17 @@
 package com.totvs.taskmanager.controller.dto.request;
 
 import com.totvs.taskmanager.enums.TaskStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "Request to update task status")
 public record UpdateTaskStatusRequest(
+
+        @Schema(
+                description = "New status of the task",
+                example = "COMPLETED",
+                allowableValues = {"PENDING", "IN_PROGRESS", "COMPLETED"}
+        )
         @NotNull(message = "Status is required")TaskStatus status
         ) {
 }
